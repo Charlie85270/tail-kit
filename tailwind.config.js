a391a80cd95878156fb87d5c8875022f362e1d57
Charlie RABILLER <charlie.rabiller@niji.fr>
2020-12-11 09:51:35 +0100

    fix purge css conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    important: true,
-    purge: {
-        content: ['./pages/**/*.tsx'],
-    },
-    theme: {},
-    variants: {},
-    plugins: [],
-    future: {
-        purgeLayersByDefault: true,
-    },
+  important: true,
+  purge: {
+    content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+  },
+  theme: {},
+  variants: {},
+  plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+  },
 };
