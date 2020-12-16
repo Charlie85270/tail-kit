519b42205bc67aaf6d511aee9726f1d7c0943702
Charlie RABILLER <charlie.rabiller@niji.fr>
2020-12-16 21:07:11 +0100

    feat(dark): add dark mode

--- components/kit/components/form/toggle/Toggle.tsx
+++ components/kit/components/form/toggle/Toggle.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import _uniqueId from "lodash/uniqueId";
 
 interface Props {
   onChange: (checked) => void;
@@ -7,19 +8,20 @@ interface Props {
 }
 
 const Toggle = (props: Props) => {
+  const [id] = useState(_uniqueId("prefix-"));
   return (
     <div>
       <div className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
         <input
           type="checkbox"
           name="toggle"
-          id="toggle"
+          id={id}
           checked={props.check}
           onChange={(e) => props.onChange(e.target.checked)}
           className="checked:right-0 checked:bg-blue-600 absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"
         />
         <label
-          htmlFor="toggle"
+          htmlFor={id}
           className="block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"
         />
       </div>
