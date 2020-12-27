d26a474044e79649536f3680e183e8594faa440e
Charlie RABILLER <charlie.rabiller@niji.fr>
2020-12-27 20:32:28 +0100

    fix(ddm): fix ddm icon

--- components/kit/components/elements/ddm/DropDownMenuWithForm.tsx
+++ components/kit/components/elements/ddm/DropDownMenuWithForm.tsx
@@ -24,11 +24,17 @@ const DropDownMenuWithForm = (props: Props) => {
         >
           {props.label}
 
-          <i
-            className={`${props.icon || "fas fa-caret-down"}${
-              props.label ? " ml-4 h-4" : " h-8 w-8 text-lg"
-            }`}
-          />
+          {props.icon || (
+            <svg
+              width="20"
+              height="20"
+              fill="currentColor"
+              viewBox="0 0 1792 1792"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M1408 704q0 26-19 45l-448 448q-19 19-45 19t-45-19l-448-448q-19-19-19-45t19-45 45-19h896q26 0 45 19t19 45z" />
+            </svg>
+          )}
         </button>
       </div>
 
--- components/site/home/HomeComps.tsx
+++ components/site/home/HomeComps.tsx
@@ -13,13 +13,11 @@ import InfoNumberCard from "../../kit/components/elements/data/InfoNumberCard";
 import CookieAlert2 from "../../kit/components/elements/alert/CookieAlert2";
 import PricingCard2 from "../../kit/components/commerce/pricing/PricingCard2";
 import ConfirmationCard from "../../kit/components/elements/alert/ConfirmationCard";
-import PricingCard8 from "../../kit/components/commerce/pricing/PricingCard8";
-import ShoppingColorChoice from "../../kit/components/commerce/shopping/ShoppingColorChoice";
 import HeadProfil from "../../kit/components/pagesection/profile/HeadProfil";
 
 const HomeComps: FC = () => {
   return (
-    <div className="opacity-10 z-10 md:z-50 md:opacity-70 sm:text-center  absolute perspective lg:text-left transform  w-full right-0  md:right-20 -top-72  md:w-1/2">
+    <div className="opacity-10 z-10 md:z-50 md:opacity-70 sm:text-center  absolute perspective lg:text-left transform  w-full -right-10 -top-72  md:w-1/2">
       <div className="flex gap-4 gap-y-1">
         <div className="col-1">
           <div className="mb-4">
