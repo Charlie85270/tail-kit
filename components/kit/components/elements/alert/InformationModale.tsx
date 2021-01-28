0b697418b327b0a8018e1d295725a780ad8ef42a
Amaan Ahmad <amaanahmad582001@gmail.com>
2021-01-28 20:25:46 +0530

    typo on button in "Modale with footer actions"

--- components/kit/components/elements/alert/InformationModale.tsx
+++ components/kit/components/elements/alert/InformationModale.tsx
@@ -51,7 +51,7 @@ const InformationModale = ({ children, withCloseBtn, onClose, withFooter }) => {
                     type="button"
                     className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
                   >
-                    Desactivate
+                    Deactivate
                   </button>
                   <button
                     type="button"
