507c1b8cf08020c92fd60c0f64950028296e864c
Sanyam Jain <xerosanyam@gmail.com>
2021-07-06 17:18:18 +0530

    fixes #65

--- components/kit/components/pagesection/team/ShadowTeams.tsx
+++ components/kit/components/pagesection/team/ShadowTeams.tsx
@@ -6,7 +6,7 @@ const ShadowTeams = () => {
         <div className="p-4">
             <p className="text-center text-3xl font-bold text-gray-800">Professional team</p>
             <p className="text-center mb-32 text-xl font-normal text-gray-500">Meat the best team in wolrd</p>
-            <div className="flex items-center flex-col md:flex-row justify evenly">
+            <div className="flex items-center space-y-24 md:space-y-0 flex-col md:flex-row justify evenly">
                 <ShadowTeam
                     img="/images/person/1.jpg"
                     name="Patrick Sebastien"
